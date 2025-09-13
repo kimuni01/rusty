02798d8b76b6349506c0828fc73e38489fe6eccd
kimuni01 <minhowin1@naver.com>
2025-09-13 14:40:38 +0900

    Update main.rs
    program v1 to v2

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,7 +1,12 @@
 use std::io; // bringing input output library
+use rand::Rng; // library for random numbers. trait that defined multiple methods. // v2
 
 fn main() { // the "entry point" of the program
     println!("Guess the number!"); // println! means it's a macro
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // v2
+
+    println!("The secret number is : {secret_number}"); // v2
 
     println!("Please input your guess."); // println! is not a function
 
