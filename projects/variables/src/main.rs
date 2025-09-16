8c6509a85470ce3bdbf5dc54cd66b6b40e130bad
kimuni01 <minhowin1@naver.com>
2025-09-16 22:58:08 +0900

    Update main.rs

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,6 +1,20 @@
 fn main() {
-    let mut x = 5; // v1 to v2
-    println!("The value of x is : {x}");
-    x = 6;
-    println!("The value of x is : {x}"); // .v1
+    // let mut x = 5; // v1 to v2
+    // println!("The value of x is : {x}");
+    // x = 6;
+    // println!("The value of x is : {x}"); // .v1
+
+    
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; // constant variable example
+
+    let x = 5; // v3
+    
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is : {x}");
+    }
+
+    println!("The value of x is : {x}"); // .v3
 }
