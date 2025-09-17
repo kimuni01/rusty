34693e9a14217a2eabfdc4399c9ee3b23ad189aa
kimuni01 <minhowin1@naver.com>
2025-09-18 06:08:49 +0900

    Update main.rs

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -7,22 +7,26 @@ fn main() {
 
     // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; // constant variable example
 
-    let x = 5; // v3
+    // let x = 5; // v3
     
-    let x = x + 1;
+    // let x = x + 1;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is : {x}");
-    }
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is : {x}");
+    // }
 
-    println!("The value of x is : {x}"); // .v3
+    // println!("The value of x is : {x}"); // .v3
 
 
     // let spaces = "    "; // v4
     // let spaces = spaces.len(); // .v4
 
-    let mut spaces = "    "; // v5
-    spaces = spaces.len(); // .v5
+    // let mut spaces = "    "; // v5
+    // spaces = spaces.len(); // .v5
+
+
+    let x = 2.0; // f64 // .v6
+    let y : f32 = 3.0; // f32
 
 }
