621f21c1b6793f73cbdf575962ecd3a0db744e80
kimuni01 <minhowin1@naver.com>
2025-09-18 07:32:06 +0900

    Add files via upload

---
+++ projects/functions/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function();
+}
+
+fn another_function() { // rust does not care the location of functions.
+    println!("Another function.");
+}
