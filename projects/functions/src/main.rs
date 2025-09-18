363f00289c51ff4f94b335ac7facfe84e151d753
kimuni01 <minhowin1@naver.com>
2025-09-18 19:56:01 +0900

    Update main.rs

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -13,8 +13,11 @@ fn main() {
 
     // println!("The value of y is : {y}"); // .v5
 
-    let x = five(); // v6
-    println!("The value of x is : {x}"); //.v6
+    // let x = five(); // v6
+    // println!("The value of x is : {x}"); //.v6
+
+    let x = plus_one(5); // v7
+    println!("The value of x is : {x}");
 }
 
 // fn another_function(x: i32) { // rust does not care the location of functions. // .v2
@@ -25,6 +28,11 @@ fn main() {
 //     println!("The measurement is : {value}{unit_label}"); // .v3
 // }
 
-fn five() -> i32 { // should a function return a value, it has to write "-> (type)" // v6
-    5 // no 'function calls', no 'macros', no 'let statement'. and this is perfectly fine.
-} // could have "return" before end of the function but most funcs end with an expression .v6
+// fn five() -> i32 { // should a function return a value, it has to write "-> (type)" // v6
+//     5 // no 'function calls', no 'macros', no 'let statement'. and this is perfectly fine.
+// } // could have "return" before end of the function but most funcs end with an expression .v6
+
+fn plus_one(x: i32) -> i32 { // v7
+    x + 1
+//    x + 1; // making an error, as this is a statement // this at the end makes func return ()
+} // .v7
