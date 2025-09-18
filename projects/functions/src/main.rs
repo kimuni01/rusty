0146b11949daf5ef9573d83ba930aaea7b78361f
kimuni01 <minhowin1@naver.com>
2025-09-18 19:42:43 +0900

    Update main.rs

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -6,12 +6,15 @@ fn main() {
 
     // let x = (let y = 6); // this is making an error and a warning // v4
 
-    let y = { // in Rust, expressions and statements are differentiated // v5
-        let x = 3; // this line is a statement, not returning any value
-        x + 1 // without this line, y is apparently empty, causing error for println!
-    }; // expressions must not end with a semicolon(;), otherwise become statements
+    // let y = { // in Rust, expressions and statements are differentiated // v5
+    //    let x = 3; // this line is a statement, not returning any value
+    //    x + 1 // without this line, y is apparently empty, causing error for println!
+    // }; // expressions must not end with a semicolon(;), otherwise become statements
 
-    println!("The value of y is : {y}"); // .v5
+    // println!("The value of y is : {y}"); // .v5
+
+    let x = five(); // v6
+    println!("The value of x is : {x}"); //.v6
 }
 
 // fn another_function(x: i32) { // rust does not care the location of functions. // .v2
@@ -21,3 +24,7 @@ fn main() {
 // fn print_labeled_measurement(value: i32, unit_label: char) { // v3
 //     println!("The measurement is : {value}{unit_label}"); // .v3
 // }
+
+fn five() -> i32 { // should a function return a value, it has to write "-> (type)" // v6
+    5 // no 'function calls', no 'macros', no 'let statement'. and this is perfectly fine.
+} // could have "return" before end of the function but most funcs end with an expression .v6
