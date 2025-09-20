98d64738be031d3149e2f7b9ad86e04671cc9386
kimuni01 <minhowin1@naver.com>
2025-09-20 13:24:40 +0900

    Add files via upload

---
+++ projects/branches/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let number = 3; // v1
+
+    // if number < 5 {
+    //     println!("condition was true");
+    // }
+    // else {
+    //     println!("condition was false");
+    // } // .v1
+
+    if number != 0 { // v2
+        println!("number was something other than zero");
+    } // .v2
+}
