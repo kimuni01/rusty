42c954fba1c28cbba2e3c3e21e757abef3a14ef6
kimuni01 <minhowin1@naver.com>
2025-09-20 13:33:32 +0900

    Update main.rs

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -12,18 +12,25 @@ fn main() {
     //     println!("number was something other than zero");
     // } // .v2
 
-    let number = 6; // v3
 
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    }
-    else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    }
-    else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    }
-    else {
-        println!("number is not divisible by 4, 3 or 2");
-    } // .v3
+    // let number = 6; // v3
+
+    // if number % 4 == 0 {
+    //     println!("number is divisible by 4");
+    // }
+    // else if number % 3 == 0 {
+    //     println!("number is divisible by 3");
+    // }
+    // else if number % 2 == 0 {
+    //     println!("number is divisible by 2");
+    // }
+    // else {
+    //     println!("number is not divisible by 4, 3 or 2");
+    // } // .v3
+
+
+    let condition = true; // v4
+    let number = if condition {5} else {6};
+
+    println!("The value of number is : {number}"); // .v4
 }
