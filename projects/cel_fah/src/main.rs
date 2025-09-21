235f5e8911fee6f57ac09bb6e4646e6f8fdc9081
kimuni01 <minhowin1@naver.com>
2025-09-21 21:38:48 +0900

    Update main.rs

--- projects/cel_fah/src/main.rs
+++ projects/cel_fah/src/main.rs
@@ -1,7 +1,7 @@
 // a program that converts fahrenheit to celsius
 // Formula: °C = (°F - 32) ÷ 1.8
 use std::io;
-// 2nd attempt failed, str1.trim() makes it '&str' while 'String' is expected
+// 3rd attempt, working but print! is faulty
 // getting assists from Claude Sonnet 4
 fn main() {
     let mut str1 = String::new();
@@ -12,20 +12,30 @@ fn main() {
         .read_line(&mut str1)
         .expect("Failed to read line");
 
-    let mut flt1:f64;
+    let mut flt1: f64; // compiler suggests the mut is not necessary, but anyways
     
-    str1 = str1.trim(); // if not done, it is "0\n" or "1\n" for expected inputs
+    str1 = str1.trim().to_string(); // if not done, it is "0\n" or "1\n" for expected inputs
 
-    if (str1 == "0") {
+    if str1 == "0" { // I prefer () for 'if' but this time.
         print!("Enter Fahrenheit Temperature : ");
-        io::stdin().read_line(&mut str1).expect("Failed to read line");
-        flt1 = str1.trim().parse().expect("Given text was not a number");
-        println!("Fahrenheit to Celsius : {0}", ((flt1 - 32.0) / 1.8));
+        let mut temp_input = String::new(); // use new variable for temperature input // v3
+        io::stdin().read_line(&mut temp_input).expect("Failed to read line");
+        flt1 = temp_input.trim().parse().expect("Given text was not a number"); // .v3
+
+        // io::stdin().read_line(&mut str1).expect("Failed to read line"); // v2
+        // flt1 = str1.trim().parse().expect("Given text was not a number"); // .v2
+
+        println!("Fahrenheit to Celsius : {}", ((flt1 - 32.0) / 1.8)); // {0} not necessary
     } // I had to change '32' to '32.0' unless Rust compiler refuses 'float + integer'
     else if (str1 == "1") {
         print!("Enter Celsius Temperature : ");
-        io::stdin().read_line(&mut str1).expect("Failed to read line");
-        flt1 = str1.trim().parse().expect("Given text was not a number");
+        let mut temp_input = String::new(); // v3
+        io::stdin().read_line(&mut temp_input).expect("Failed to read line");
+        flt1 = temp_input.trim().parse().expect("Given text was not a number"); // .v3
+
+        // io::stdin().read_line(&mut str1).expect("Failed to read line"); // v2
+        // flt1 = str1.trim().parse().expect("Given text was not a number"); // .v2
+
         println!("Celsius to Fahrenheit : {0}", ((flt1 * 1.8) + 32.0));
     }
 }
