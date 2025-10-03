62db3e334f89c2dec551fcdf625b87b119651edf
kimuni01 <minhowin1@naver.com>
2025-10-03 17:05:19 +0900

    Update main.rs

--- projects/twelveda/src/main.rs
+++ projects/twelveda/src/main.rs
@@ -1,7 +1,6 @@
 // a program for printing lyrics of the song
 // "The Twelve Days of Christmas", utilizing it's repetitiveness
-// 2nd attempt, under immense pressure of making it in first time,
-// I forgot adding some newlines.
+// 3rd attempt, there was only one missing thing - 0..i to 0..=i.
 
 fn main() {
     let lyr = ["And a partridge in a pear tree.",
@@ -22,7 +21,7 @@ A partridge in a pear tree.\n");
     for i in (1..=11) {
         println!("On the {} day of Christmas,", lyr2[i]);
         println!("My true love sent to me");
-        for j in (0..i).rev() {
+        for j in (0..=i).rev() { // to 0..=i, otherwise 2nd and 12th are omitted
             println!("{}", lyr[j]);
         }
         println!();
