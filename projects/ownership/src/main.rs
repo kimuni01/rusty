00d7c107ffc455ec4157ba1180a8d011f2009d61
kimuni01 <minhowin1@naver.com>
2025-10-04 23:21:58 +0900

    Add files via upload

---
+++ projects/ownership/src/main.rs
@@ -0,0 +1,5 @@
+fn main() { // 's' is not yet defined, hence invalid at here. // v1
+    let s = "hello"; // since this point, 's' is valid. // .v1
+
+    // do something with 's'.
+} // this scope has ended, 's' is no longer valid.
