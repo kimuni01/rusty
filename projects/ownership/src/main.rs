6fa23dd1db8009a4fb39aa1f6842a21a17b849c6
kimuni01 <minhowin1@naver.com>
2025-10-04 23:23:59 +0900

    Update main.rs

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,5 +1,9 @@
 fn main() { // 's' is not yet defined, hence invalid at here. // v1
-    let s = "hello"; // since this point, 's' is valid. // .v1
+    // let s = "hello"; // since this point, 's' is valid. // .v1
 
     // do something with 's'.
+
+    let mut s = String::from("hello"); // v2
+    s.push_str(", world!"); // push_str() adds literal to the String.
+    println!("{}", s); // this lline prints 'hello, world!' // .v2
 } // this scope has ended, 's' is no longer valid.
