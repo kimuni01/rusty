60ac352dfd63dcb96f005ec60a8c0432620eb720
kimuni01 <minhowin1@naver.com>
2025-10-06 18:07:46 +0900

    Update main.rs

--- projects/ownership3/src/main.rs
+++ projects/ownership3/src/main.rs
@@ -1,12 +1,19 @@
 fn main() { // v1
     let s1 = String::from("hello");
-    let (s2, len) = calculate_length(s1);
-    println!("The length of '{}' is {}.", s2, len);
+    // let (s2, len) = calculate_length(s1);
+    // println!("The length of '{}' is {}.", s2, len);
+
+    let len = calculate_length(&s1); // v2
+    println!("The length of '{}' is {}.", s1, len); // .v2
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len(); // len() returns the length of String.
+// fn calculate_length(s: String) -> (String, usize) {
+//     let length = s.len(); // len() returns the length of String.
 
-    (s, length)
-} // .v1
+//     (s, length)
+// } // .v1
 
+fn calculate_length(s: &String) -> usize { // s is the reference of String. // v2
+    s.len();
+} // from here s is out of the scope. however it does not own what it refers, // .v2
+// hence original String is not dropped.
