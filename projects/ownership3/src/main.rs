008f4ed84963b71a18a285a148b6a2e9169f33e1
kimuni01 <minhowin1@naver.com>
2025-10-06 18:12:58 +0900

    Update main.rs

--- projects/ownership3/src/main.rs
+++ projects/ownership3/src/main.rs
@@ -1,10 +1,13 @@
 fn main() { // v1
-    let s1 = String::from("hello");
+    // let s1 = String::from("hello");
     // let (s2, len) = calculate_length(s1);
     // println!("The length of '{}' is {}.", s2, len);
 
-    let len = calculate_length(&s1); // v2
-    println!("The length of '{}' is {}.", s1, len); // .v2
+    // let len = calculate_length(&s1); // v2
+    // println!("The length of '{}' is {}.", s1, len); // .v2
+
+    let s = String::from("hello"); // v3
+    change(&s); // .v3
 }
 
 // fn calculate_length(s: String) -> (String, usize) {
@@ -13,7 +16,11 @@ fn main() { // v1
 //     (s, length)
 // } // .v1
 
-fn calculate_length(s: &String) -> usize { // s is the reference of String. // v2
-    s.len();
-} // from here s is out of the scope. however it does not own what it refers, // .v2
+// fn calculate_length(s: &String) -> usize { // s is the reference of String. // v2
+    // s.len();
+// } // from here s is out of the scope. however it does not own what it refers, // .v2
 // hence original String is not dropped.
+
+fn change(some_string: &String) { // v3
+    some_string.push_str(", world"); // this cannot work, reference cannot change the value
+} // .v3
