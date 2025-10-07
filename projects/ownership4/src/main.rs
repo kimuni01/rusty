b64abac40fb195147107df5d7db5aa4a0aff24a1
kimuni01 <minhowin1@naver.com>
2025-10-08 00:19:54 +0900

    Update main.rs

--- projects/ownership4/src/main.rs
+++ projects/ownership4/src/main.rs
@@ -1,10 +1,17 @@
 fn main() {
-    let reference_to_nothing = dangle(); // v1
+    // let reference_to_nothing = dangle(); // v1 .v1
+    let real_string = no_dangle(); // v2
 }
 
-fn dangle() -> &String { // dangle returns reference of String
-    let s = String::from("hello"); // s is a new String
+// fn dangle() -> &String { // dangle returns reference of String // v1
+    // let s = String::from("hello"); // s is a new String
 
-    &s //returns a reference of String
-} // on here s is out of scope and dropped. its memory is released // .v1
+    // &s //returns a reference of String
+// } // on here s is out of scope and dropped. its memory is released // .v1
 // this is dangerous
+
+fn no_dangle() -> String { // v2
+    let s = String::from("hello");
+
+    s
+} // .v2
