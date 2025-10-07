2dce4c13905865ad74705bf9fd5abc609d1bd664
kimuni01 <minhowin1@naver.com>
2025-10-08 00:50:56 +0900

    Update main.rs

--- projects/ownership4/src/main.rs
+++ projects/ownership4/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     // let reference_to_nothing = dangle(); // v1 .v1
-    let real_string = no_dangle(); // v2
+    // let real_string = no_dangle(); // v2
+
+    let mut s = String::from("hello world"); // v4
+    let word = first_word(&s); // word receives value 5
+    s.clear(); // this code empties String and make ""
+    // from here, word still has 5 but the String that lets 5 usable
+    // does not exist. word is not valid at all now // .v4
 }
 
 // fn dangle() -> &String { // dangle returns reference of String // v1
@@ -10,11 +16,11 @@ fn main() {
 // } // on here s is out of scope and dropped. its memory is released // .v1
 // this is dangerous
 
-fn no_dangle() -> String { // v2
-    let s = String::from("hello");
+// fn no_dangle() -> String { // v2
+    // let s = String::from("hello");
 
-    s
-} // .v2
+    // s
+// } // .v2
 
 fn first_word(s: &String) -> usize { // v3
     let bytes = s.as_bytes();
