a8c3c29fb03d17e6af5844e3be27b1988cea1bab
kimuni01 <minhowin1@naver.com>
2025-10-10 02:42:29 +0900

    Update main.rs

--- projects/structs1/src/main.rs
+++ projects/structs1/src/main.rs
@@ -5,11 +5,28 @@ struct User { // v1
     sign_in_count: u64,
 } // .v1
 
-fn main() { // v2
-    let user1 = User {
+fn main() {
+    let user1 = User {  // v2
         sign_in_count: 1,
         active: true,
         username: String::from("someusername123"),
         email: String::from("someone@example.com"),
+    }; // .v2
+
+    let mut user1 = User { // v3
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
     };
-} // .v2
+    user1.email = String::from("anotheremail@example.com"); // .v3
+}
+
+fn build_user(email: String, username: String) -> User { // v3
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    } // no semicolon for an expression
+} // .v3
