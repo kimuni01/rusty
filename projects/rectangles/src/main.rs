e8ef7ce450142979302c4baec86c00172150b1dc
kimuni01 <minhowin1@naver.com>
2025-10-11 23:09:12 +0900

    Update main.rs

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -12,11 +12,13 @@ fn main() {
         height: 50,
     }; // .v3
 
+    println!("rect1 is {}", rect1); // does not work yet // v9 .v9
+
     println!(
         "The area of the rectangle is {} square pixels.",
         // area(width1, height1)
         // area(rect1) // introducing a tuple here // v2 .v2
-        area(&rect1)
+        area(&rect1) // only borrowing the ownership here // v3 .v3
     ); // .v1
 }
 
@@ -28,6 +30,6 @@ fn main() {
 //     dimensions.0 * dimensions.1
 // } // .v2
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+fn area(rectangle: &Rectangle) -> u32 { // accessing the borrowed instance field does not // v3
+    rectangle.width * rectangle.height // move the field value, hence borrowing of
+} // structs are often found. // .v3
