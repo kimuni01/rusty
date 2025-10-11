fb8a4b98040c17a1e89886785e2cbead7fd92a7c
kimuni01 <minhowin1@naver.com>
2025-10-11 22:54:43 +0900

    Update main.rs

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -4,10 +4,15 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        // area(width1, height1)
+        area(rect1) // v2 .v2
     ); // .v1
 }
 
-fn area(width: u32, height: u32) -> u32 { // v1
-    width * height
-} // .v1
+// fn area(width: u32, height: u32) -> u32 { // v1
+//     width * height
+// } // .v1
+
+fn area(dimensions: (u32, u32)) -> u32 { // v2
+    dimensions.0 * dimensions.1
+} // .v2
