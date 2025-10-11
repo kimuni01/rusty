2cf9b4271a8df5c5bc59d39d8d3855a9819702c2
kimuni01 <minhowin1@naver.com>
2025-10-11 23:00:13 +0900

    Update main.rs

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,11 +1,22 @@
+struct Rectangle { // introducing a struct here v3
+    width: u32,
+    height: u32,
+} // .v3
+
 fn main() {
-    let width1 = 30; // v1
-    let height1 = 50;
+    // let width1 = 30; // v1
+    // let height1 = 50;
+
+    let rect1 = Rectangle { // v3
+        width: 30,
+        height: 50,
+    }; // .v3
 
     println!(
         "The area of the rectangle is {} square pixels.",
         // area(width1, height1)
-        area(rect1) // v2 .v2
+        // area(rect1) // introducing a tuple here // v2 .v2
+        area(&rect1)
     ); // .v1
 }
 
@@ -13,6 +24,10 @@ fn main() {
 //     width * height
 // } // .v1
 
-fn area(dimensions: (u32, u32)) -> u32 { // v2
-    dimensions.0 * dimensions.1
-} // .v2
+// fn area(dimensions: (u32, u32)) -> u32 { // v2
+//     dimensions.0 * dimensions.1
+// } // .v2
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
